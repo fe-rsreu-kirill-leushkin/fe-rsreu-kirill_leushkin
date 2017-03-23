4a1ece8d1a7e5d457a5b85589a1bf6c0f560db4b
fe-rsreu-kirill-leushkin <kirill.lichtenberg@gmail.com>
2017-03-23 16:27:59 +0300

    Update script.js

--- CSS_BASICS/parallax/script.js
+++ CSS_BASICS/parallax/script.js
@@ -4,20 +4,12 @@ function parallax () {
 	y = event.clientY / 2;
 	x = event.clientX / 2;
 	
-	
 	for (var i = 1; i < 8; i++){
-		//src="images/layer-07.png"
 		image = document.getElementById('image' + i);
-		console.log('image' + i);
 		image.style.top = y * i * 0.01 + 'px';
 		image.style.left = x * i * 0.01 + 'px';
-		console.log(image);
 	}
-	
-	
 	image = document.getElementById('image');
-	
-	//console.log(image.style.top += 10 + 'px');
 }
 
 window.addEventListener('mousemove', parallax);
