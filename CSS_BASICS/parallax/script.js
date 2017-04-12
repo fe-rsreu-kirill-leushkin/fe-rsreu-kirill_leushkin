7ae0e5eb5113dee9f19b4e8b1b0da786ff503e65
fe-rsreu-kirill-leushkin <kirill.lichtenberg@gmail.com>
2017-04-12 14:09:29 +0300

    Add files via upload

--- CSS_BASICS/parallax/script.js
+++ CSS_BASICS/parallax/script.js
@@ -1,13 +1,18 @@
 var y, image;
 
 function parallax () {
-	y = event.clientY / 2;
-	x = event.clientX / 2;
+	console.log(document.documentElement.clientHeight);
+	
+	y = (event.clientY - document.documentElement.clientHeight) / 2 ;
+	x = (event.clientX - document.documentElement.clientHeight) / 2;
 	
 	for (var i = 1; i < 8; i++){
 		image = document.getElementById('image' + i);
-		image.style.top = y * i * 0.01 + 'px';
-		image.style.left = x * i * 0.01 + 'px';
+		image.style.top = (-1) * y * i * 0.01 + 'px';
+		image.style.left = (-1) * x * i * 0.01 + 'px';
+		
+		image.style.transform = 'translateY(' + (-1) * y * i * 0.001 + 'px)';
+		image.style.transform = 'translateX(' + (-1) * x * i * 0.001 + 'px)';
 	}
 	image = document.getElementById('image');
 }
