63d75f055458bdb95efd20818d3929e26362772e
fe-rsreu-kirill-leushkin <kirill.lichtenberg@gmail.com>
2017-04-13 09:47:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 (function () {
 	'use strict';
-
+	
 	function showVariables(a, b) {
-		window.console.log(a, b);
+		console.log(a, b);
 	}
 
 	function trangleArea(bottom, height) {
+		if (bottom < 0 || height < 0) {
+			return 0;
+		}
+		
 		return 0.5 * (bottom * height);
 	}
 
-	function arrayReverse(array) {
+	function arrayReverseFor(array) {
 		var i = 0;
 		var j = array.length - 1;
 		var temp;
+		
+		
+		if (!Array.isArray(array)) {
+			return 0;
+		}
 		
 		for (i, j; i < j; i++, j--) {
 			temp = array[j];
 			array[j] = array[i];
 			array[i] = temp;
 		}
+		return array;
+	}
+
+	function arrayReverseWhile(array) {
+		var i = 0;
+		var j = array.length - 1;
+		var temp;
 		
-		/*
+		
+		if (!Array.isArray(array)) {
+			return 0;
+		}
+
 		do {
 			
 			temp = array[j];
@@ -31,8 +51,21 @@
 			j--;
 			
 		} while ( i < j );
+
+		return array;
+	}
+
+
+	function arrayReverseDoWhile(array) {
+		var i = 0;
+		var j = array.length - 1;
+		var temp;
 		
 		
+		if (!Array.isArray(array)) {
+			return 0;
+		}
+
 		while ( i < j ) {
 			temp = array[j];
 			array[j] = array[i];
@@ -41,13 +74,17 @@
 			i++;
 			j--;
 		}
-		*/
 
 		return array;
 	}
 
 	function differenceIncDec(i) {
 		var temp = i;
+		
+		if (typeof i != 'number') {
+			return 0;
+		}
+		
 		window.console.log('your variable: ' + i);
 		window.console.log('variable i after increment: ' + (i++) + '??? the same ???');
 		window.console.log('but your variable has changed: ' + i);
@@ -56,13 +93,11 @@
 	}
 
 	function isPosNeg() {
-		var i = 0;
-		for (i; i < arguments.length; i++) {
-
-			if (typeof arguments[i] !== 'number') {
-				window.console.log('argument[' + i + '] is NAN');
-				continue;
-			}
+		if (typeof arguments[0] != 'number') {
+			return 0;
+		}
+		
+		for (var i = 0; i < arguments.length; i++) {
 
 			if (arguments[i] < 0) window.console.log('argument[' + i + '] is negative value');
 			
@@ -80,20 +115,22 @@
 	}
 
 	function factorial( value ) {
+		if (typeof value != 'number') {
+			return 0;
+		}
+		
 		return (value !== 1) ? value * factorial( value - 1 ) : 1;
 	}
-	
-	
-	
 	
 	var one = 1, two = 2;
 
 	showVariables(one, two);
 	window.console.log(trangleArea(3, 4));
-	window.console.log(arrayReverse([1, 2, 3, 4, 5, 6, 7, 8]));
+	window.console.log(arrayReverseFor([1, 2, 3, 4, 5, 6, 7, 8]));
+	window.console.log(arrayReverseWhile([1, 2, 3, 4, 5, 6, 7, 8]));
+	window.console.log(arrayReverseDoWhile([1, 2, 3, 4, 5, 6, 7, 8]));
 	differenceIncDec(5);
-	isPosNeg(1, -1, '5', 0.1);
+	isPosNeg(1);
 	askName();
 	window.console.log(factorial(3));
-	
 }());
