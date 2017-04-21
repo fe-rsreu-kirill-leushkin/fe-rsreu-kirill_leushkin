a5ca61af171c638e371c8becdc32d7c743895017
fe-rsreu-kirill-leushkin <kirill.lichtenberg@gmail.com>
2017-04-21 10:01:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,68 +2,90 @@ function isNumber(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-var Calculator = (function () {
-  'use strict';
+var Calculator = (function (initialResult) {
+	'use strict';
 
-  var result = 0;
+	var result = initialResult;
 
-  function add(number) {
-    if (!isNumber(number)) {
-    	throw new Error('parametr(s) should be a number');
-    }
-    result += number;
-    return add;
-  }
+	function add() {
+		for (var i = 0; i < arguments.length; i++) {
+			if (!isNumber(arguments[i])) {
+				throw new Error('parametr(s) should be a number');
+			}
+		}
+		
+		for (var i = 0; i < arguments.length; i++) {
+			result += arguments[i];	
+		}
+		
+		return add;
+	}
 
-  function subtract(number) {
-    if (!isNumber(number)) {
-    	throw new Error('parametr(s) should be a number');
-    }
-    result -= number;
-    return subtract;
-  }
+	function subtract() {
+		for (var i = 0; i < arguments.length; i++) {
+			if (!isNumber(arguments[i])) {
+				throw new Error('parametr(s) should be a number');
+			}
+		}
+		
+		for (var i = 0; i < arguments.length; i++) {
+			result -= arguments[i];	
+		}
+		
+		return subtract;
+	}
 
-  function divide(number) {
-    if (!isNumber(number) || number == 0) { // || result == 0) {
-    	throw new Error('parametr(s) should be a number which biggest than 0');
-    }
-    }
-    result /= number;
-    return divide;
-  }
+	function divide() {
+		for (var i = 0; i < arguments.length; i++) {
+			if (!isNumber(arguments[i])) {
+				throw new Error('parametr(s) should be a number');
+			}
+		}
+		
+		for (var i = 0; i < arguments.length; i++) {
+			result /= arguments[i];	
+		}
+		
+		return divide;
+	}
 
-  function multiply(number) {
-    if (!isNumber(number)) {
-    	throw new Error('parametr(s) should be a number');
-    }
-    result *= number;
-    return multiply;
-  }
+	function multiply() {
+		for (var i = 0; i < arguments.length; i++) {
+			if (!isNumber(arguments[i])) {
+				throw new Error('parametr(s) should be a number');
+			}
+		}
+		
+		for (var i = 0; i < arguments.length; i++) {
+			result /= arguments[i];	
+		}
+		
+		return multiply;
+	}
 
-  function getResult() {
-    return result;
-  }
+	function getResult() {
+		return result;
+	}
 
-  function reset() {
-    return result = 0;
-  }
+	function reset() {
+		return result = 0;
+	}
 
-  return {
-    add: add,
-    subtract: subtract,
-    divide: divide,
-    multiply: multiply,
-    getResult: getResult,
-    reset: reset
-  }
+	return {
+		add: add,
+		subtract: subtract,
+		divide: divide,
+		multiply: multiply,
+		getResult: getResult,
+		reset: reset
+	};
+}(50));
 
-}());
-
-Calculator.add(1)(2)(1)(4);
+Calculator.add(1, 1)(2, 2)(1, 9)(4);
 console.log(Calculator.getResult());
-Calculator.subtract(2);
+Calculator.subtract(2)(2,2);
 console.log(Calculator.getResult());
 Calculator.divide(2);
 console.log(Calculator.getResult());
-Calculator.multiply(3);
+Calculator.multiply(3, 1.5);
 console.log(Calculator.getResult());
