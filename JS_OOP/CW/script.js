203b44ffa4cb167380971bdec5571e03b41a3ab2
fe-rsreu-kirill-leushkin <kirill.lichtenberg@gmail.com>
2017-06-02 08:57:41 +0300

    OOP

--- JS_OOP/CW/script.js
+++ JS_OOP/CW/script.js
@@ -1,6 +1,5 @@
 /*jshint esversion: 6 */
-
-(function() {
+(function(){
   'use strict';
 
   /*
@@ -8,14 +7,72 @@
   getPerimeter() and draw() method. Draw method shows in console that (*name is drawn)
   */
 
-  function Shape(type, name) {
-    this.type = type || '';
-    this.name = name || '';
+  // TODO: Add setters
+  // TODO: Use ES6 classes
+  class Shape1 {
+    constructor(type, name) {
+      this.type = type || '';
+      this.name = name || '';
+    }
+
+    setType(type) {
+      if (typeof type === 'string') {
+        this.type = type;
+      } else {
+        throw new Error('type should be a string');
+      }
+    }
+
+    getType() {
+      return this.type;
+    }
+
+    draw() {
+      return [this.type, this.name];
+    }
+
+    getPerimetr() {
+      var per = 0;
+      for (let i = 0; i < arguments.length; i++) {
+        per += arguments[i];
+      }
+
+      return per;
+    }
   }
 
-  Shape.prototype.getType = function() {
-    return this.type;
-  };
+  // TODO: Use private variables
+  function Shape(type, name) {
+    var _type;
+    var _name;
+
+    this.getType = function () {
+      return _type;
+    };
+
+    this.setType = function(newType) {
+      if (typeof newType !== 'string') {
+        throw new Error('type must be a string');
+      } else {
+        _type = newType;
+      }
+    };
+
+    this.getName = function() {
+      return _name;
+    };
+
+    this.setName = function(newName) {
+      if (typeof newType !== 'string') {
+        throw new Error('type must be a string');
+      } else {
+        _name = newName;
+      }
+    };
+
+    this.setType(type);
+    this.setName(name);
+  }
 
   Shape.prototype.getPerimetr = function() {
     var per = 0;
@@ -36,18 +93,45 @@
   representing the lengths of the sides of a triangle
   */
 
+  // TODO: Use Object.defineProperties() for getters and setters
   function Traingle(a, b, c) {
+    Shape.apply(this, ['traingle']);
+
+    Object.defineProperties(this, {
+      a: {
+        get: function() {
+          return a;
+        },
+        set: function(a) {
+          if (a < 0) {
+            throw new Error('traingles side must be a positive number');
+          } else if (typeof a !== 'number') {
+            throw new Error('traingle side must be a number');
+          }
+        }
+      },
+
+      b: {
+        get: function() {
+          return b;
+        },
+        set: function(b) {
+          if (b < 0) {
+            throw new Error('traingles side must be a positive number');
+          } else if (typeof b !== 'number') {
+            throw new Error('traingle side must be a number');
+          }
+        }
+      },
+    });
+
     this.a = a || 0;
     this.b = b || 0;
     this.c = c || 0;
-
-    // setters and getters
-    // private variables
-    // Object.defineProperties()
-
   }
 
   Traingle.prototype = Object.create(Shape.prototype);
+  Traingle.constructor = Traingle;
 
 
   /*
@@ -57,19 +141,37 @@
   */
 
   function Square(d) {
+    Shape.apply(this, ['traingle']);
     Traingle.apply(this, arguments);
     this.d = d || 0;
   }
 
   Square.prototype = Object.create(Shape.prototype);
+  Traingle.constructor = Traingle;
 
   /*
   4)Add one more Shape that inherits Triangle or Square or some other shape
   (in this case you should add it too) and has some specific method and properties
   */
 
+  function Shape2(v) {
+    Traingle.apply(this, arguments);
+    this.v = v || 0;
+  }
+
+  Shape2.prototype.showPer = function() {
+    var per = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      per += arguments[i];
+    }
+
+    return console.log(per);
+  };
+
+  var s = new Shape({}); 
   var square = new Square();
   var traingle = new Traingle();
   console.log(square.getPerimetr(1,2,3,4), traingle.getPerimetr(1,2,3));
-  console.dir(Shape);
+
+  traingle.b = -100;
 }());
